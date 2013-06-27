3698d87b95e5412a4f801da0750eeb4789864c13
Oleksandr Antonyuk <kunotna@gmail.com>
2013-06-27 17:27:28 +0300

    Add simple html in admin base template and included t.bootstrap

--- application/views/admin/base.php
+++ application/views/admin/base.php
@@ -1,2 +1,14 @@
-Base template Admin
-<h1><?=$title?></h1>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Шаблон</title>
+    <!-- Bootstrap -->
+    <link href="/media/css/bootstrap.min.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <h1><?=$title?></h1>
+    
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="/media/js/bootstrap.min.js"></script>
+  </body>
+</html>
