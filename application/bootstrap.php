9a78abc4542f22ac5a131f3a1acd65d9af2c5bec
Oleksandr Antonyuk <kunotna@gmail.com>
2013-06-27 14:04:12 +0300

    Add base_url /

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -90,7 +90,7 @@ if (isset($_SERVER['KOHANA_ENV']))
  * - boolean  expose      set the X-Powered-By header                        FALSE
  */
 Kohana::init(array(
-	'base_url'   => '/kohana/',
+	'base_url'   => '/',
     'errors'     => TRUE, //This will convert PHP errors into exceptions which are easier to handle
 ));
 
