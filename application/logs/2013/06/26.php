6fc0d930ce8eb171a5f51efb8f5adad3e22f0cb9
Olexandr <kunotna@gmail.com>
2013-07-01 08:12:49 +0300

    Add unicorn template

---
+++ application/logs/2013/06/26.php
@@ -0,0 +1,29 @@
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
+
+2013-06-26 21:15:33 --- CRITICAL: ErrorException [ 4 ]: syntax error, unexpected 'exit' (T_EXIT), expecting function (T_FUNCTION) ~ SYSPATH\classes\HTTP\Exception\404.php [ 3 ] in :
+2013-06-26 21:15:33 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2013-06-26 21:15:33 --- CRITICAL: ErrorException [ 4 ]: syntax error, unexpected 'exit' (T_EXIT), expecting function (T_FUNCTION) ~ SYSPATH\classes\HTTP\Exception\404.php [ 3 ] in :
+2013-06-26 21:15:33 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2013-06-26 21:15:33 --- CRITICAL: ErrorException [ 4 ]: syntax error, unexpected 'exit' (T_EXIT), expecting function (T_FUNCTION) ~ SYSPATH\classes\HTTP\Exception\404.php [ 3 ] in :
+2013-06-26 21:15:33 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2013-06-26 21:17:26 --- CRITICAL: ErrorException [ 4 ]: syntax error, unexpected '1' (T_LNUMBER) ~ SYSPATH\classes\HTTP\Exception\404.php [ 2 ] in :
+2013-06-26 21:17:26 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2013-06-26 21:17:26 --- CRITICAL: ErrorException [ 4 ]: syntax error, unexpected '1' (T_LNUMBER) ~ SYSPATH\classes\HTTP\Exception\404.php [ 2 ] in :
+2013-06-26 21:17:26 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2013-06-26 21:17:26 --- CRITICAL: ErrorException [ 4 ]: syntax error, unexpected '1' (T_LNUMBER) ~ SYSPATH\classes\HTTP\Exception\404.php [ 2 ] in :
+2013-06-26 21:17:26 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2013-06-26 21:21:01 --- CRITICAL: ErrorException [ 1 ]: Call to undefined function ext() ~ APPPATH\classes\HTTP\Exception\404.php [ 2 ] in :
+2013-06-26 21:21:01 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2013-06-26 21:21:01 --- CRITICAL: ErrorException [ 1 ]: Call to undefined function ext() ~ APPPATH\classes\HTTP\Exception\404.php [ 2 ] in :
+2013-06-26 21:21:01 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
+2013-06-26 21:21:01 --- CRITICAL: ErrorException [ 1 ]: Call to undefined function ext() ~ APPPATH\classes\HTTP\Exception\404.php [ 2 ] in :
+2013-06-26 21:21:01 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in :
