f63932be5e834db610e44baebadabe9a9f33a5c5
Oleksandr Antonyuk <kunotna@gmail.com>
2013-07-03 16:51:45 +0300

    Redefined db driver from Mysql to PDO

--- application/config/database.php
+++ application/config/database.php
@@ -2,28 +2,27 @@
 
 return array
 (
-	'default' => array
-	(
-		'type'       => 'MySQL',
+	'default' => array(
+		'type'       => 'PDO',
 		'connection' => array(
 			/**
-			 * The following options are available for MySQL:
+			 * The following options are available for PDO:
 			 *
-			 * string   hostname     server hostname, or socket
-			 * string   database     database name
-			 * string   username     database username
-			 * string   password     database password
-			 * boolean  persistent   use persistent connections?
-			 * array    variables    system variables as "key => value" pairs
-			 *
-			 * Ports and sockets may be appended to the hostname.
+			 * string   dsn         Data Source Name
+			 * string   username    database username
+			 * string   password    database password
+			 * boolean  persistent  use persistent connections?
 			 */
-			'hostname'   => 'localhost',
-			'database'   => 'blog',
+			'dsn'        => 'mysql:host=localhost;dbname=blog',
 			'username'   => 'root',
 			'password'   => '',
 			'persistent' => FALSE,
 		),
+		/**
+		 * The following extra options are available for PDO:
+		 *
+		 * string   identifier  set the escaping identifier
+		 */
 		'table_prefix' => '',
 		'charset'      => 'utf8',
 		'caching'      => FALSE,
